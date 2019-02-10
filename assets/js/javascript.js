0804df3bbbf8eb61d8a4cece2acd08735cb41fd5
T4T4el7 <ochoaarmando13@gmail.com>
2019-02-09 20:41:29 -0800

    linked to firebose a simple system for messaging as well as displaying my email and phone info on the contact page

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,1 +1,28 @@
 
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCUhBTS3JndJU6Ai8GWyUl0udbBxjHwDWY",
+    authDomain: "testing-600b3.firebaseapp.com",
+    databaseURL: "https://testing-600b3.firebaseio.com",
+    projectId: "testing-600b3",
+    storageBucket: "testing-600b3.appspot.com",
+    messagingSenderId: "200196981739"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+
+  $("#send").on("click", function(event) {
+    event.preventDefault();
+    var nombre = $("#nombre").val().trim();
+    var mensage = $("#mensage").val().trim();
+    var email = $("#email").val().trim();
+    var personUno = nombre;
+    database.ref().set({
+        Person: nombre,
+        Message: mensage,
+        Mail: email
+    });
+
+  });

07a96bb428cbf3ce12809747de469b656c01f0f1
T4T4el7 <ochoaarmando13@gmail.com>
2020-01-02 18:16:55 -0800

    major changes to the overall aesthetic of my portfolio, now have cleaned up and made to look more impressive, will continue to play around with the css and lay out further

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,30 +1,7 @@
+let loader = document.getElementById("loader");
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCUhBTS3JndJU6Ai8GWyUl0udbBxjHwDWY",
-    authDomain: "testing-600b3.firebaseapp.com",
-    databaseURL: "https://testing-600b3.firebaseio.com",
-    projectId: "testing-600b3",
-    storageBucket: "testing-600b3.appspot.com",
-    messagingSenderId: "200196981739"
-  };
-  firebase.initializeApp(config);
-
-  var database = firebase.database();
-
-
-  $("#send").on("click", function(event) {
-    event.preventDefault();
-    var nombre = $("#nombre").val().trim();
-    var mensage = $("#mensage").val().trim();
-    var email = $("#email").val().trim();
-
-    var Persona = {
-        Nombre : nombre,
-        Mensage : mensage,
-        EMail : email
-    };
-
-    database.ref().push(Persona);
-
-  });
+window.addEventListener("load", function(event) {
+    loader.classList.remove("loading");
+    loader.classList.add("loaded");
+    document.body.classList.add("imgloaded");
+})
